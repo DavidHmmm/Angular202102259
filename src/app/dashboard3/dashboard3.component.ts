30066b4c52e91ad5a7cd2b57121b33f1636df148
DavidHmmm <dvincentd366@gmail.com>
2022-10-18 20:35:01 +0700

    Update

--- src/app/dashboard3/dashboard3.component.ts
+++ src/app/dashboard3/dashboard3.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+declare const $ : any;
+declare const Plugin: any;
 
 @Component({
   selector: 'app-dashboard3',
   templateUrl: './dashboard3.component.html',
   styleUrls: ['./dashboard3.component.css']
 })
-export class Dashboard3Component implements OnInit {
+export class Dashboard3Component implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private renderer : Renderer2) { }
 
+  ngAfterViewInit(): void{
+    this.renderer.removeClass(document.body,"login-page");
+    this.renderer.removeClass(document.body,"sidebar-open");
+    this.renderer.addClass(document.body,"sidebar-closed");
+
+    $('[data-widget="treeview"]').Treeview("init");
+    Plugin.call($(this))
+  }  
   ngOnInit(): void {
   }
 
