4747466a28293e72975d9cbe802db85e7d7866b3
DavidHmmm <dvincentd366@gmail.com>
2022-10-18 20:17:08 +0700

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { ContentComponent } from './content/content.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
@@ -11,6 +13,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
   {path: "dashboard", component: DashboardComponent},
+  {path: "dashboard2", component: Dashboard2Component},
+  {path: "dashboard3", component: Dashboard3Component},
   {path: "login", component:LoginComponent},
   {path: "register", component:RegisterComponent},
   {path: "admin", component:AdminComponent},
